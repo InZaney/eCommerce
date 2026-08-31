5ccda4580b491562428117657c269b5e95fa7152
Zane Marcoe <zmarcoe5840@students.cptc.edu>
2026-08-31 10:51:58 -0700

    Add Product model with validation and documentation
    Introduced a Product class in the eCommerce.Models namespace with properties for ProductId, Title, and Price. Applied data annotations for validation and included XML documentation for clarity.

---
+++ eCommerce/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents an individaul product for sale
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// The unique identifier for the product
+        /// </summary>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// The user facing title of the product
+        /// </summary>
+        [StringLength(50)]
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// The current sales price of the product
+        /// </summary>
+        [Range(0, 10000)]
+        public double Price { get; set; }
+    }
+}
