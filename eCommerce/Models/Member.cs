1ea312515144880f4f4be17d7423c31d7668b66e
Zane Marcoe <zmarcoe5840@students.cptc.edu>
2026-08-31 13:45:57 -0700

    Add Member model with user properties and annotations
    Introduced the Member class in the eCommerce.Models namespace to represent website users. The model includes MemberId, Username, Email, Password, and DateOfBirth properties, with data annotations for keys and required fields, and XML documentation for clarity.

---
+++ eCommerce/Models/Member.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerce.Models
+{
+    /// <summary>
+    /// Represents an individual website user
+    /// </summary>
+    public class Member
+    {
+        /// <summary>
+        /// Unique identifier for the member
+        /// </summary>
+        [Key]
+        public int MemberId { get; set; }
+
+        /// <summary>
+        /// Public facing username for the member.
+        /// Alphanumeric characters only
+        /// </summary>
+        public required string Username { get; set; }
+
+        /// <summary>
+        /// Email for the member
+        /// </summary>
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Member's password
+        /// </summary>
+        public required string Password { get; set; }
+
+        /// <summary>
+        /// Date of birth of the member
+        /// </summary>
+        public DateOnly DateOfBirth { get; set; }
+    }
+}
