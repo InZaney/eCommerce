fc8e0632d41179920708df0889f8f1fe0827b10e
Zane Marcoe <zmarcoe5840@students.cptc.edu>
2026-08-31 14:09:56 -0700

    Add RegistrationViewModel with validation attributes
    Introduced RegistrationViewModel in eCommerce.Models to support user registration. Includes Username, Email, Password, ConfirmPassword, and DateOfBirth properties with validation annotations for input correctness.

--- eCommerce/Models/Member.cs
+++ eCommerce/Models/Member.cs
@@ -37,4 +37,38 @@ namespace eCommerce.Models
         /// </summary>
         public DateOnly DateOfBirth { get; set; }
     }
+
+    public class RegistrationViewModel
+    {
+        /// <summary>
+        /// Public facing username for the member.
+        /// Alphanumeric characters only
+        /// </summary>
+        [RegularExpression(@"^[a-zA-Z0-9]+$", ErrorMessage = "Username can only contain alphanumeric characters.")]
+        [StringLength(25)]
+        public required string Username { get; set; }
+
+        /// <summary>
+        /// Email for the member
+        /// </summary>
+        [DataType(DataType.EmailAddress)]
+        public required string Email { get; set; }
+
+        /// <summary>
+        /// Member's password
+        /// </summary>
+        [StringLength(50, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 50 characters.")]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public required string ConfirmPassword { get; set; }
+
+        /// <summary>
+        /// Date of birth of the member
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateOnly DateOfBirth { get; set; }
+    }
 }
